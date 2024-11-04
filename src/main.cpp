5f437a252c07ed9e2210544e260672f65b9c2082
David <daviddahoui09@gmail.com>
2024-11-04 14:37:03 +0100

    perfect tiles

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 int main() {
     //Game game;
     //game.startGame();
-    Game mold;
-    mold.displayMold(40);
+    /*Game mold;
+    for (int i=0; i < 96; i++) {
+        mold.displayMold(i);
+    }*/
 
     return 0;
 }
